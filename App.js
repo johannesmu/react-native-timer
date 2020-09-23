975dcd10ed6572990a53e50240c59cddcfd5e100
johannes <jmuljana@gmail.com>
2020-09-24 00:30:48 +1000

    working timer with useEffect

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
 
 export default function App() {
+  const [time, setTime] = useState(0)
+  const [paused, setPaused] = useState(true)
+
+  // global reference for timer
+  let timer = null
+
+  // useEffect( () => {
+  //   timer = setInterval( () => { setTime( time + 1 ) } , 1000)
+  // })
+
+  useEffect(() => {
+    if (!paused) {
+      timer = setInterval(() => {
+        setTime(time => time + 1);
+      }, 1000)
+      return () => clearInterval(timer)
+    }
+  })
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.time}>{time} seconds</Text>
+      <TouchableOpacity style={styles.button} onPress={() => {
+        setPaused(paused ? false : true)
+      }} >
+        <Text>{paused ? "Start" : "Stop"}</Text>
+      </TouchableOpacity>
+      <View style={styles.row}>
+        <TouchableOpacity style={[{ display: paused && time > 0 ? "flex" : "none" }, styles.save]}>
+          <Text style={styles.saveText}>Delete</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[{ display: paused && time > 0 ? "flex" : "none" }, styles.save]}>
+          <Text style={styles.saveText}>Save</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -17,5 +47,29 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  time: {
+    fontSize: 32,
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: 'yellow',
+    padding: 15,
+    minWidth: 100,
+  },
+  row: {
+    flexDirection: 'row',
+    position: 'absolute',
+    bottom: 15,
+    justifyContent: 'space-between',
+    padding: 10,
+    width: '100%',
+  },
+  save: {
+    backgroundColor: 'darkblue',
+    padding: 15,
+  },
+  saveText: {
+    color: 'white',
   },
 });
